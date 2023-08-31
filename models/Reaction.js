ea019b2ad212854b24a1c17eff68bc4ffc45ef3f
Steve Hudek <sjhudek@gmail.com>
2023-08-30 18:14:38 -0600

    modified models/Reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+// const ReactionSchema = require('./Thought');
+
+const ReactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+});
+
+module.exports = ReactionSchema;
