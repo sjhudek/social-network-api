c78489a6c116a8b353e6087d6009ce5aa82f3c0a
Steve Hudek <sjhudek@gmail.com>
2023-08-30 18:13:35 -0600

    modified utils/data.js

---
+++ utils/data.js
@@ -0,0 +1,35 @@
+module.exports = {
+    users: [
+        {
+            username: 'john_doe',
+            email: 'john_doe@example.com',
+        },
+        {
+            username: 'jane_doe',
+            email: 'jane_doe@example.com',
+        },
+        // ... Add more users as needed
+    ],
+    thoughts: [
+        {
+            thoughtText: 'This is my first thought!',
+            username: 'john_doe'
+        },
+        {
+            thoughtText: 'I love coding!',
+            username: 'jane_doe'
+        },
+        // ... Add more thoughts as needed
+    ],
+    reactions: [
+        {
+            reactionBody: 'Great thought!',
+            username: 'jane_doe'
+        },
+        {
+            reactionBody: 'Thanks!',
+            username: 'john_doe'
+        },
+        // ... Add more reactions as needed
+    ]
+};
