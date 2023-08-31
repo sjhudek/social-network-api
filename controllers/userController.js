0769ece1865b1636a6a0e0435d6eef93e46931d1
Steve Hudek <sjhudek@gmail.com>
2023-08-30 18:09:31 -0600

    modified controllers/userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+const Thought = require('../models/Thought');
+
+module.exports = {
+    getAllUsers: async () => {
+        return await User.find().populate('thoughts').populate('friends');
+    },
+    getUserById: async (id) => {
+        return await User.findById(id).populate('thoughts').populate('friends');
+    },
+    createUser: async (data) => {
+        const newUser = new User(data);
+        return await newUser.save();
+    },
+    updateUser: async (id, data) => {
+        return await User.findByIdAndUpdate(id, data, { new: true });
+    },
+    deleteUser: async (id) => {
+        const userToDelete = await User.findById(id);
+        await Thought.deleteMany({ _id: { $in: userToDelete.thoughts } });
+        return await userToDelete.delete();
+    }
+};
