bd29a838ef4d72e2a4534a46a5ef76d03373dac1
Steve Hudek <sjhudek@gmail.com>
2023-08-30 18:05:56 -0600

    modified app.js

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const app = express();
 
-const app = express();  //Create new instance
+// Just require the connection.js here; it will automatically connect to MongoDB
+require('./config/connection');
 
-mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
-        console.log("Database connection Success!");
-    }).catch((err) => {
-        console.error("MongoDB Connection Error", err);
-    });
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000; //Declare the port number
+const userRoutes = require('./routes/api/users');
+const thoughtRoutes = require('./routes/api/thoughts');
 
-app.use(express.json()); //allows us to access request body as req.body
-app.use(morgan("dev"));  //enable incoming request logging in dev mode
- 
-//Define the endpoint
-app.get("/ping", (req, res) => {  
-  return res.send({
-    status: "Healthy",
-  });
-});
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server started listening on port : ", PORT);
+    console.log(`Server is running on port ${PORT}`);
 });
-
