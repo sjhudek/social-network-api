889b0f523af6fe3c2979e2f81bebc8223f578700
Steve Hudek <sjhudek@gmail.com>
2023-09-01 15:46:15 -0600

    deleted routes/api/reactions.js

--- routes/api/reactions.js
+++
@@ -1,9 +0,0 @@
-// routes/api/reactions.js
-const router = require('express').Router();
-const reactionController = require('../../controllers/reactionController');
-
-router.post('/', async (req, res) => {
-    res.json(await reactionController.createReaction(req.body));
-});
-
-module.exports = router;
