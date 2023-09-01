2d9a748c477e210d50e00a774b1552ef9ef0a824
Steve Hudek <sjhudek@gmail.com>
2023-09-01 16:44:47 -0600

    updated controllers/thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,11 +1,18 @@
 const Thought = require('../models/Thought');
 
 addReaction: async (thoughtId, reactionData) => {
-    const thought = await Thought.findById(thoughtId);
-    thought.reactions.push(reactionData);
-    await thought.save();
-    return thought;
-};
+    try {
+        const thought = await Thought.findById(thoughtId);
+        if (!thought) {
+            throw new Error('No thought found with this id!');
+        }
+        thought.reactions.push(reactionData);
+        await thought.save();
+        return thought;
+    } catch (error) {
+        throw error;
+    }
+}
 
 removeReaction: async (thoughtId, reactionId) => {
     const thought = await Thought.findById(thoughtId);
@@ -30,5 +37,32 @@ module.exports = {
     },
     deleteThought: async (id) => {
         return await Thought.findByIdAndDelete(id);
-    }
+    },
+    addReaction: async (thoughtId, reactionData) => {
+        try {
+            const thought = await Thought.findById(thoughtId);
+            if (!thought) {
+                throw new Error('No thought found with this id!');
+            }
+            thought.reactions.push(reactionData);
+            await thought.save();
+            return thought;
+        } catch (error) {
+            throw error;
+        }
+    },
+    removeReaction: async (thoughtId, reactionId) => {
+        try {
+            const thought = await Thought.findById(thoughtId);
+            if (!thought) {
+                throw new Error('No thought found with this id!');
+            }
+            thought.reactions.id(reactionId).remove();
+            await thought.save();
+            return thought;
+        } catch (error) {
+            throw error;
+        }
+    }    
 };
+
