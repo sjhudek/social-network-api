dfd73ab32660dc0c387aa725605478e96cdbca13
Steve Hudek <sjhudek@gmail.com>
2023-09-01 18:23:42 -0600

    refactored models/User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    _id: {
-        type: Number,
-        required: true
-    },
     username: {
         type: String,
         unique: true,
