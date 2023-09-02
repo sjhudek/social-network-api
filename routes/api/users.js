e778e26ba05cce506c5f0f5ecaeb6b69f6bfca78
Steve Hudek <sjhudek@gmail.com>
2023-09-01 18:24:38 -0600

    refactoredroutes/api/users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,4 +21,25 @@ router.delete('/:id', async (req, res) => {
     res.json(await userController.deleteUser(req.params.id));
 });
 
+// Add a friend
+router.put('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const userWithNewFriend = await userController.addFriend(req.params.userId, req.params.friendId);
+        res.json(userWithNewFriend);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Remove a friend
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const userWithoutFriend = await userController.removeFriend(req.params.userId, req.params.friendId);
+        res.json(userWithoutFriend);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 module.exports = router;
