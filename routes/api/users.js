3a17bedf61f961bc23a699d4c3f5c9c1ec5fa7fd
Steve Hudek <sjhudek@gmail.com>
2023-09-01 21:05:37 -0600

    updated routes/api/users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,6 +13,7 @@ router.post('/', async (req, res) => {
     res.json(await userController.createUser(req.body));
 });
 
+// Use PUT for updating a user
 router.put('/:id', async (req, res) => {
     res.json(await userController.updateUser(req.params.id, req.body));
 });
@@ -21,8 +22,8 @@ router.delete('/:id', async (req, res) => {
     res.json(await userController.deleteUser(req.params.id));
 });
 
-// Add a friend
-router.put('/:userId/friends/:friendId', async (req, res) => {
+// Add a friend using POST
+router.post('/:userId/friends/:friendId', async (req, res) => {
     try {
         const userWithNewFriend = await userController.addFriend(req.params.userId, req.params.friendId);
         res.json(userWithNewFriend);
@@ -31,7 +32,7 @@ router.put('/:userId/friends/:friendId', async (req, res) => {
     }
 });
 
-// Remove a friend
+// Remove a friend using DELETE
 router.delete('/:userId/friends/:friendId', async (req, res) => {
     try {
         const userWithoutFriend = await userController.removeFriend(req.params.userId, req.params.friendId);
@@ -40,6 +41,5 @@ router.delete('/:userId/friends/:friendId', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
 
 module.exports = router;
