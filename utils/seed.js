f6d3e9648cdc56f293f9441e73ef90059264f360
Steve Hudek <sjhudek@gmail.com>
2023-09-01 21:06:36 -0600

    updated utils/seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,43 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId; // Moved this line after requiring mongoose
-const { users, thoughts, reactions } = require('./data');
-const User = require('../models/User');
-const Thought = require('../models/Thought');
-require('../config/connection');
+const { users, thoughts, reactions } = require("./data");
+const User = require("../models/User");
+const Thought = require("../models/Thought");
+require("../config/connection");
 
 const seedDatabase = async () => {
     try {
-        console.log("Seeding database...");
-    await User.deleteMany({});
-    await Thought.deleteMany({});
-    
-    const mappedUsers = users.map(({ id, ...rest }) => {
-        return {
-            ...rest,
-            _id: id
-        };
-    });
-    
-    const mappedThoughts = thoughts.map(({ id, ...rest }) => {
-        return {
-            ...rest,
-            _id: id
-        };
-    });         
-
-    // Place the console.log statements here
-    console.log(mappedUsers);
-    console.log(mappedThoughts);
-
-    await User.insertMany(mappedUsers);
-    await Thought.insertMany(mappedThoughts);
-
-    console.log('All data imported!');
-    process.exit(0);
-
-} catch (error) {
-    console.error("Error seeding database:", error);
-}
-};
+      console.log("Seeding database...");
+      
+      // Clear existing data
+      await User.deleteMany({});
+      await Thought.deleteMany({});
+  
+      // Insert new data
+      await User.insertMany(users);
+      await Thought.insertMany(thoughts);
+  
+      console.log("All data imported!");
+      process.exit(0);
+    } catch (error) {
+      console.error("Error seeding database:", error);
+    }
+  };
+  
 
 seedDatabase();
