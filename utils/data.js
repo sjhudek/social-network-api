e8af2bdca197838acf05d9a2c330f61d8d35be57
Steve Hudek <sjhudek@gmail.com>
2023-09-01 21:06:08 -0600

    updated utils/data.js

--- utils/data.js
+++ utils/data.js
@@ -3,62 +3,50 @@ module.exports = {
         {
             username: 'carmine_dioxide',
             email: 'carmine_dioxide@example.com',
-            id: 1
         },
         {
             username: 'lou_segusi',
             email: 'lou_segusi@example.com',
-            id: 2
         },
         {
             username: 'hugo_girl',
             email: 'hugo_girl@example.com',
-            id: 3
         },
         {
             username: 'barbara_seville',
             email: 'barbara_seville@example.com',
-            id: 4
         },
         {
             username: 'placebo_domingo',
             email: 'placebo_domingo@example.com',
-            id: 5
         },
         {
             username: 'noah_vale',
             email: 'noah_vale@example.com',
-            id: 6
         },
     ],
     thoughts: [
         {
-            id: 1,
             thoughtText: 'This is my first thought!',
-            userName: 'carmine_dioxide'
+            username: 'carmine_dioxide'
         },
         {
-            id: 2,
             thoughtText: 'I love coding!',
-            userName: 'lou_segusi'
+            username: 'lou_segusi'
         },
         {
-            id: 3,
             thoughtText: 'Exploring new places is so exciting!',
-            userName: 'hugo_girl'
+            username: 'hugo_girl'
         },
         {
-            id: 4,
             thoughtText: 'Trying out new recipes in the kitchen today.',
-            userName: 'barbara_seville'
+            username: 'barbara_seville'
         },
         {
-            id: 5,
             thoughtText: 'Opera is the best!',
             username: 'placebo_domingo'
         },
         {
-            id: 6,
             thoughtText: 'Couch sufer.',
             username: 'noah_vale'
         },
